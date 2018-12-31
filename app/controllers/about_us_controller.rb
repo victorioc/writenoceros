56d0497e4c7b90c15cf733ca20bfdebfa116d7d0
“wesseljt” <jwessel@freshwatersystems.com>
2018-12-31 15:44:53 -0500

    wip on about us, got clearfix working

---
+++ app/controllers/about_us_controller.rb
@@ -0,0 +1,6 @@
+class AboutUsController < ApplicationController
+
+	def index
+		@active_nav= "About Us"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get root to: 'home#index'
+  get "/about", to: 'about_us#index'
 end
